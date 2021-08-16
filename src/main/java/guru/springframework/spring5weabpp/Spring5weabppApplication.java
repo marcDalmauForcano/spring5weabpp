412d7cd0da2dbea464f750a8ad5cb780384638f2
marcDalmauForcano <marcdalmauforcano@gmail.com>
2021-08-16 12:07:44 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/spring5weabpp/Spring5weabppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5weabpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5weabppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5weabppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/spring5weabpp/Spring5weabppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5weabpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5weabppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
